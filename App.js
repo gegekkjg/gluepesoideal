e574a2bdd7abcb1e30d59410e00805f3eaa64063
Geovana Rocha <133898564+gegekkjg@users.noreply.github.com>
2023-12-06 14:09:40 +0000

    gg

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
-import { GluestackUIProvider, Text, Box, Center, Heading } from '@gluestack-ui/themed';
+import { GluestackUIProvider, Image, Button, ButtonText, Text, Switch, HStack, FormControlLabelText, Box, Center, Heading, FormControl, FormControlLabel, FormControlErrorText, Input, InputField } from '@gluestack-ui/themed';
 import { useState } from "react";
 import { config } from '@gluestack-ui/config'; // Optional if you want to use default theme
 
-import homem from "./assets/homem.png"
-import mulher from "./assets/mulher.png"
+import homem from "./assets/mm-removebg-preview.png"
+import mulher from "./assets/ff-removebg-preview.png"
 
 export default function App() {
   const [peso, setPeso] = useState("72")
@@ -39,9 +39,44 @@ export default function App() {
 
   return (
     <GluestackUIProvider config={config}>
-      <Box  width="100%" h={'$full'} justifyContent="center" alignItems="center" gap={'$5'} p={'$5'}>
+      <Box p="$2" h={"#full"} w={"#full"} borderRadius={"$md"}>
         <Center  h={"$full"}>
           <Heading>Peso Ideal</Heading>
+          <FormControl>
+            <FormControlLabel>
+              <FormControlErrorText>Peso</FormControlErrorText>
+            </FormControlLabel>
+            <Input w={100} h={35}>
+              <InputField value={peso} onChangeText={setPeso} keyboardType='numeric'/>
+            </Input>
+            <FormControlLabel>
+              <FormControlErrorText>Altura</FormControlErrorText>
+            </FormControlLabel>
+            <Input w={100} h={35}>
+              <InputField value={altura} onChangeText={setAltura} keyboardType='number-pad'/>
+            </Input>
+            <FormControlLabel>
+                  <FormControlLabelText>Sexo</FormControlLabelText>
+                </FormControlLabel>
+                <HStack w={"$full"} space="md" justifyContent="center" alignItems="center">
+                    <FormControlLabelText>Homem</FormControlLabelText>
+                    <Switch value={sexo} onValueChange={setSexo} />
+                    <FormControlLabelText>Mulher</FormControlLabelText>
+                </HStack>
+                <Box display='flex' flexDirection='row' justifyContent='space-between'>
+                <Button onPress={calcularHandle} bg='#5361ac'>
+                    <ButtonText>Calcular</ButtonText>
+                </Button>
+                <Button onPress={limparHandle} bg='#5361ac'>
+                    <ButtonText>Limpar</ButtonText>
+                </Button>
+                <Box justifyContent="space-between" alignItems="center" height={100} flexDirection="row">
+                    <Text>Resultado</Text>
+                    <Text fontWeight="$bold">{resultado}</Text>
+                </Box>
+                </Box>
+          </FormControl>
+          <Image source={sexo ? mulher : homem} alt="imagem do tipo do sexo"/>
         </Center>
       </Box>
     </GluestackUIProvider>
